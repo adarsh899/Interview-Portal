fd970f14c501b9676413ffa82133fbf80fc9e3a5
Adarsh_819 <mishraadarsh.899@gmail.com>
2021-11-09 18:08:51 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const config = require('./config/mongoose')
 const cors = require('cors')
 const routes = require('./routes')
+const path = require('path');
 app.use(cors())
 app.use(express.urlencoded());
 app.use(express.json());
@@ -11,6 +12,9 @@ app.use('/', routes);
 const PORT = process.env.PORT || 5000
 if (process.env.NODE_ENV == "production") {
     app.use(express.static(" frontend/my-app/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,  "frontend","my-app","build", "index.html"));
+      });
     
 }
 app.listen(PORT, (err) => {
